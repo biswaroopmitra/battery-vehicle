f9865fb51c6ff188c938737f9ed666e1ec38b8f9
roopmitra86 <roopmitra86@gmail.com>
2026-03-31 02:07:02 +0530

    First commit

--- src/main/java/com/example/BatteryStateOfHealth/Battery/authentication/validation/AuthConstants.java
+++
@@ -1,8 +0,0 @@
-package com.example.BatteryStateOfHealth.Battery.authentication.validation;
-
-public class AuthConstants {
-    public static final String BEARER = "Bearer ";
-    public static final String AUTH_CONTROLLER = "/authentication";
-    public static final int AUTH_PORT = 0;
-    public static final String AUTH_VALIDATION_ENDPOINT = "/validation";
-}
