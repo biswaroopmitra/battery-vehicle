c08c720437923a2c12f386cf211fc876ef4578aa
roopmitra86 <roopmitra86@gmail.com>
2026-04-08 01:52:00 +0530

    vehicle brand package

--- src/main/java/com/example/BatteryStateOfHealth/Battery/vehiclebrand/VehicleBrand.java
+++ src/main/java/com/example/BatteryStateOfHealth/Battery/vehiclebrand/VehicleBrand.java
@@ -1,8 +1,58 @@
 package com.example.BatteryStateOfHealth.Battery.vehiclebrand;
 
 import com.example.BatteryStateOfHealth.Battery.baseEntity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class VehicleBrand extends BaseEntity {
+    @NotNull(message = "name is required")
+    @NotBlank(message = "name is required")
+    @Column(nullable = false)
+    String name;
+
+    @NotNull(message = "code is required")
+    @NotBlank(message = "code is required")
+    @Column(nullable = false)
+    String code;
+
+    @NotNull(message = "active is required")
+    @NotBlank(message = "active is required")
+    @Column(nullable = false)
+    Boolean active;
+
+    public VehicleBrand() {
+    }
+
+    public VehicleBrand(String name, String code, Boolean active) {
+        this.name = name;
+        this.code = code;
+        this.active = active;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }
---
+++ src/main/java/com/example/BatteryStateOfHealth/Battery/vehiclebrand/VehicleBrandRepository.java
@@ -0,0 +1,6 @@
+package com.example.BatteryStateOfHealth.Battery.vehiclebrand;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleBrandRepository extends JpaRepository<VehicleBrand, Long> {
+}
