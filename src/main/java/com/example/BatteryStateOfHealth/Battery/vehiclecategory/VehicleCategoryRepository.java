298af2550a680cb2cec65aa1e91246abac5807bd
roopmitra86 <roopmitra86@gmail.com>
2026-04-12 01:13:33 +0530

    packages: vehicle, vehicletype, vehiclecategory

---
+++ src/main/java/com/example/BatteryStateOfHealth/Battery/vehiclecategory/VehicleCategoryRepository.java
@@ -0,0 +1,5 @@
+package com.example.BatteryStateOfHealth.Battery.vehiclecategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleCategoryRepository extends JpaRepository<VehicleCategory, Long> {
+}
